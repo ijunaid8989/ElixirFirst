17f3b1c7611bb6039bd943c59a70e0bff0a05273
Junaid Farooq <junaid@evercam.io>
2016-04-28 12:58:37 +0500

    Added lib/issues.ex

---
+++ issues/lib/issues/cli.ex
@@ -0,0 +1,35 @@
+defmodule Issues.CLI do
+  @defult_count 4
+
+  @moduledoc """
+  It will handle Command line parsing of option to the project
+  """
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  argv can be -h or something which reflect to help otherwise will work with tuples of user
+  project and count
+  """
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv, switches: [help: :boolean],
+                                     aliases: [h: :help])
+
+    case parse do
+
+      { [help: true ], _, _ } 
+        -> :help
+
+      { _, [user, project, count], _ }
+        -> {user, project, count}
+
+      { _, [user, project], _}
+        -> {user, project, @defult_count}
+
+      _ -> :help
+    end
+  end
+end
